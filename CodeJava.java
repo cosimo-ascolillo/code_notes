5fef2d6bc6370e9b1c56f7fa160cdbcee10910e5
cosimo-ascolillo <cosimo.ascolillo@gmail.com>
2026-08-26 10:52:59 +0200

    Update CodeJava.java

--- CodeJava.java
+++ CodeJava.java
@@ -1,1 +1,14 @@
-
+class CodeJava {
+  
+  /* Enum Utils: extract value of from string */
+  private static <E extends Enum<E>> E safeValueOf(final Class<E> enumClass, final String value) {
+    if (value == null || value.isBlank()) {
+      return null;
+    }
+    try {
+      return Enum.valueOf(enumClass, value.trim());
+    } catch (final IllegalArgumentException e) {
+      return null;
+    }
+  }
+}
